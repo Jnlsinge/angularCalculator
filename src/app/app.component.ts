5f254106ef10e02f541b56769ad37f1004aefc0b
J'Neil Bering <jcstyleshop08@gmail.com>
2021-10-04 23:09:55 +0800

    angularCalcu staged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCalculator';
+
+  number1 = 0;
+  number2 = 0;
+  result = 0;
+
+  num1(value : any){
+    this.number1 = parseInt(value);
+    console.log(this.number1);
+  }
+
+  num2(value : any){
+    this.number2 = parseInt(value);
+    console.log(this.number2)
+  }
+
+  onClickAddition(){
+    this.result = (this.number1 + this.number2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  onClickSubtraction(){
+    this.result = (this.number1 - this.number2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  onClickMultiplication(){
+    this.result = (this.number1 * this.number2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
+
+  onClickDivision(){
+    this.result = (this.number1 / this.number2);
+    let finalResult = document.getElementById("result") as HTMLInputElement;
+    finalResult.valueAsNumber = this.result;
+  }
 }
+
+
+
